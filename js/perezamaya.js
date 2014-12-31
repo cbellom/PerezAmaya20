0891f12bd42ded4527bac1ec830a3bbfaa2dfa36
cbellom <cbellom@unal.edu.co>
2014-12-30 22:56:19 -0500

    init

---
+++ js/perezamaya.js
@@ -0,0 +1,51 @@
+jQuery(function($)
+{
+    $("#contact_form").submit(function()
+    {
+        var name = $("#name").val(); // get name field value
+        var tpid = $("#tpid").val(); // get tpid field value
+        var id = $("#id").val(); // get id field value
+        var phone = $("#phone").val(); // get phone field value
+        var email = $("#email").val(); // get email field value
+        var subject = $("#subject").val(); // get email field value
+        var msg = $("#msg").val(); // get message field value
+
+        $.ajax(
+            {
+                type: "POST",
+                url: "https://mandrillapp.com/api/1.0/messages/send.json",
+                data: {
+                    'key': 'pSD__Gy91Ba5IzfyWQdpBw',
+                    'message': {
+                        'from_email': email,
+                        'from_name': name,
+                        'headers': {
+                            'Reply-To': email
+                        },
+                        'subject': subject,
+                        "html": "<h1>Nombre: "+name +"</h1><br/><h2>Documento: "+tpid+" "+id+"</h2><br/><p>Telefono: "+phone+"<br/>Correo: "+email+"<br/><br/>Mensaje: "+msg+"</p>",
+                        'to': [
+                            {
+                                'email': 'bottestdummymail@gmail.com',
+                                'name': 'bot mail',
+                                'type': 'to'
+                            }]
+                    }
+                }
+            })
+            .done(function(response) {
+                alert('Su mensaje ha sido enviado. Gracias!!'); // show success message
+                $("#name").val(''); // reset field after successful submission
+                $("#tpid").val(''); // reset field after successful submission
+                $("#id").val(''); // reset field after successful submission
+                $("#phone").val(''); // reset field after successful submission
+                $("#email").val(''); // reset field after successful submission
+                $("#subject").val(''); // reset field after successful submission
+                $("#msg").val(''); // reset field after successful submission
+            })
+            .fail(function(response) {
+                alert('Error enviando el correo. Por favor intente mas tarde.');
+            });
+        return false; // prevent page refresh
+    });
+});
